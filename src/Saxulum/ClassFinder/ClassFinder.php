bcd9449f693e1517253960cafb04bcf1fc0b0fb7
Dominik Zogg <dominik.zogg@gmail.com>
2015-01-21 21:07:05 +0100

    prevent problems with code like MyClass::class

--- src/Saxulum/ClassFinder/ClassFinder.php
+++ src/Saxulum/ClassFinder/ClassFinder.php
@@ -14,7 +14,7 @@ class ClassFinder
 
         for ($i = 0; $i < $tokenCount; $i++) {
 
-            if (is_array($tokens[$i]) && in_array($tokens[$i][0], array(T_NAMESPACE, T_CLASS))) {
+            if (is_array($tokens[$i]) && in_array($tokens[$i][0], array(T_NAMESPACE, T_CLASS)) && $tokens[$i+1][0] === T_WHITESPACE) {
                 $type = $tokens[$i][0]; $i++; $namespace = '';
                 if ($type === T_NAMESPACE) {
                     $namespaceStack = array();
